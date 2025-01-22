d8cb6c5dda90c6d1cf4b90c8bcb153caa9b33840
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-01-22 06:56:57 +0100

    minor changes

--- src/lib.jsx
+++ src/lib.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react'
-
 //Takes a string and makes the first letter capital
 export function capitalize(val){
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
